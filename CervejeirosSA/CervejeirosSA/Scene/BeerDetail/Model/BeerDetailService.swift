d768842c1df3b779359e54da4c243950867f66c8
Adann Simões <6124486+adannsergio@users.noreply.github.com>
2023-02-21 23:34:17 -0300

    Correções apontadas pelo swiftlint

--- CervejeirosSA/CervejeirosSA/Scene/BeerDetail/Model/BeerDetailService.swift
+++ CervejeirosSA/CervejeirosSA/Scene/BeerDetail/Model/BeerDetailService.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 protocol BeerDetailProtocol {
     func fetchDetail(of beerId: Int, completion: @escaping (Result<[Beer], Error>) -> Void)
     func downloadImage(from imageUrl: URL, completion: ((Data?) -> Void)?)
@@ -15,25 +14,25 @@ protocol BeerDetailProtocol {
 
 class BeerDetailService: BeerDetailProtocol {
     let apiClient: APIClient
-    
+
     init(apiClient: APIClient = APIClient()) {
         self.apiClient = apiClient
     }
-    
+
     func fetchDetail(of beerId: Int, completion: @escaping (Result<[Beer], Error>) -> Void) {
         let endpoint: APIClient.Endpoint = .init(path: "beers/\(beerId)",
                                                  httpMethod: "GET",
                                                  queryItens: nil)
-        
+
         apiClient.request(request: endpoint.urlRequest, completion: completion)
     }
-    
+
     func downloadImage(from imageUrl: URL, completion: ((Data?) -> Void)? = nil) {
         let endpoint: APIClient.Endpoint = .init(baseUrl: imageUrl,
                                                  path: String(),
                                                  httpMethod: "GET",
                                                  queryItens: nil)
-        
+
         apiClient.downloadImageData(request: endpoint.urlRequest, completion: completion)
     }
 }
